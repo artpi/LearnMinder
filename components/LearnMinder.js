848be549a4d490e662878a3b40e3170880adab6b
artpi <artpi@post.pl>
2015-12-20 23:12:22 +0100

    Win detection for code.org

--- components/LearnMinder.js
+++ components/LearnMinder.js
@@ -3,13 +3,15 @@ import Locked from './Locked';
 import Browser from './Browser';
 import OnlineExam from './OnlineExam';
 
+const injection = "if (!winInterval) { var winInterval = setInterval( function() { if( document.querySelector('.win-feedback') ) { clearInterval( winInterval ); alert('WIN'); } } , 1000 ) };";
+
 const LearnMinder = React.createClass( {
 	getInitialState: function() {
 		return {
 			scene: 'locked',
 			url: 'http://www.wp.pl',
 			nextLesson: 'https://studio.code.org/s/starwarsblocks/stage/1/puzzle/1',
-			injectedJavaScript: 'alert("ziemniak")'
+			injectedJavaScript: injection
 		};
 	},
 	update: function( change ) {
