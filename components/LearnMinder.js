47b9797cdf197a91c232e41a8d9aebcdcc185156
artpi <artpi@post.pl>
2015-12-20 22:35:26 +0100

    Working shell

--- components/LearnMinder.js
+++ components/LearnMinder.js
@@ -9,14 +9,28 @@ var {
 } = React;
 
 import Locked from './Locked';
+import Browser from './browser';
+
 
 var LearnMinder = React.createClass({
+  getInitialState: function() {
+    return {
+      scene: 'locked',
+      url: 'http://www.wp.pl'
+    };
+  },
+  update: function( change ) {
+    this.setState( change );
+  },
   render: function() {
-    return (
-      <Locked>
-      </Locked>
-    );
+    switch( this.state.scene ) {
+      case 'locked':
+        return ( <Locked update={this.update}></Locked> );
+      case 'browser':
+        return ( <Browser update={this.update} url={ this.state.url }></Browser> );
+    }
   }
 });
+
 
 export default LearnMinder;
--- components/Locked.js
+++ components/Locked.js
@@ -1,11 +1,21 @@
 import React from 'react-native';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 var Locked = React.createClass({
+  go: function() {
+    this.props.update( {scene: 'browser'} );
+  },
   render: function() {
     return (
       <View>
         <Text>Oh noes</Text>
+
+          <TouchableOpacity
+            onPress={ this.go }>
+            <Text>
+               {'DAWAJ'}
+            </Text>
+          </TouchableOpacity>
       </View>
     );
   }
